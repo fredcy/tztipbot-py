0631c080638df334fe3abedd3c21c4c693594d6b
Fred Yankowski <fcy@imsa.edu>
2019-06-29 17:10:02 -0500

    basic chat connection

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 import logging
 import pprint
 from matrix_client.client import MatrixClient
@@ -12,10 +13,31 @@ def init():
     print(f"homeserver={homeserver}")
 
     client = MatrixClient(homeserver)
-    token = client.login(username=config.get("client", "username"),
-                         password=config.get("client", "password"))
-    print(token)
+    username=config.get("client", "username")
+    password=config.get("client", "password")
+    print(f"username={username}")
+
+    token = client.login(username=username, password=password)
+    print(f"token={token}")
+    return client
+
+
+def on_event(room, event):
+    print(f"room={room} event={json.dumps(event, indent=4)}")
 
 
 if __name__ == "__main__":
-    init()
+    client = init()
+
+    room_alias = "#TezosConcerns:matrix.org"
+    room = client.join_room(room_alias)
+    room.add_listener(on_event)
+
+    client.start_listener_thread()
+
+    while True:
+        msg = input()
+        if msg == "/quit":
+            break
+        else:
+            room.send_text(msg)
